ab473a08b3074cf3652f40513086a87a9f1c4a3f
Devon Wells <devon.wells@pm.me>
2024-12-30 08:57:14 -0500

    init deno deploy adapter and add SSR test page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+// @ts-chec
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import deno from "@deno/astro-adapter";
+
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+  site: "https://alexwells.dev",
+  integrations: [mdx(), sitemap()],
+  adapter: deno(),
 });
